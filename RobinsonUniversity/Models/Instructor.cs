d6e2aafa919c4c73cf90d88fe75c9e997b97fd62
Tanner Robinson <Rob16041@byui.edu>
2022-06-23 15:10:46 -0600

    DB complexData struct added

--- RobinsonUniversity/Models/Instructor.cs
+++ RobinsonUniversity/Models/Instructor.cs
@@ -1,34 +1,37 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace RobinsonUniversity.Models
 {
-    public class Student
+    public class Instructor
     {
         public int ID { get; set; }
+
         [Required]
         [Display(Name = "Last Name")]
         [StringLength(50)]
         public string LastName { get; set; }
+
         [Required]
+        [Column("FirstName")]
         [Display(Name = "First Name")]
         [StringLength(50)]
-        [Column("FirstName")]
         public string FirstMidName { get; set; }
-        [Required]
-        [Display(Name = "Enrollment Date")]
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
-        public DateTime EnrollmentDate { get; set; }
-        [Display(Name ="Full Name")]
+        [Display(Name = "Hire Date")]
+        public DateTime HireDate { get; set; }
+
+        [Display(Name = "Full Name")]
         public string FullName
         {
-            get
-            {
-                return LastName + ", " + FirstMidName;
-            }
+            get { return LastName + ", " + FirstMidName; }
         }
 
-        public ICollection<Enrollment>? Enrollments { get; set; }
+        public ICollection<CourseAssignment> CourseAssignments { get; set; }
+        public OfficeAssignment OfficeAssignment { get; set; }
     }
 }
